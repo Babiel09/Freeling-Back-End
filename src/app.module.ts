a8e3cc1fc707696e4b229de73ae392925dd76b8a
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-26 14:59:32 -0300

    config: Configured Bull Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { UserModule } from './user/user.module';
 import { ServicesModule } from './services/services.module';
 import { TagsModule } from './tags/tags.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { BullModule } from '@nestjs/bull';
+import { USER_QUEUE } from './constants/constants';
 
 @Module({
-  imports: [UserModule, ServicesModule, TagsModule, PrismaModule],
+  imports: [
+    UserModule, 
+    ServicesModule, 
+    TagsModule, 
+    PrismaModule,
+    BullModule.forRoot({
+      redis:{
+        host:process.env.REDIS_HOST,
+        port:Number(process.env.REDIS_PORT),
+      },
+    }),
+    BullModule.registerQueue({
+      name:USER_QUEUE
+    }),
+  ],
   controllers: [],
   providers: [],
 })
