59fb7a856b30220e2e405f6fc4e4ece7e7dbbb9d
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-26 14:33:41 -0300

    config: Imported the prisma.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ServicesModule } from './services/services.module';
 import { TagsModule } from './tags/tags.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, ServicesModule, TagsModule],
+  imports: [UserModule, ServicesModule, TagsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
