dd8fdbb74be6e2965efc9b1d7678159df93e60ab
Gabriel Castro <estudosbabiel@gmail.com>
2025-03-01 20:01:38 -0300

    feat: New method to henerate a salt of a number into 12 to 40

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get, HttpException, Logger, Post, Res } from '@nestjs/common';
+import { UserService } from './user.service';
+import * as bcrypt from 'bcrypt';
+import { Response } from 'express';
+import { CreateUserDTO } from './DTO/user.create.dto';
 @Controller('user')
-export class UserController {}
+export class UserController {
+private logger = new Logger(UserController.name)
+  constructor(private userService:UserService){};  
+
+    private async randomizeHash(stringToHash:string){
+        const saltArray:number[] = [12,14,16,18,20,22,24,26,28,30,32,34,36,38,40];
+        let salt:number =  Math.floor(Math.random() * saltArray.length);
+        let realHash = await bcrypt.hash(stringToHash,salt);
+        return realHash; 
+    };
+
+  @Get("/v1/allUsers")
+  private async getUsers(@Res()res:Response):Promise<Response>{
+    try{
+        const allUsers = await this.userService.showAllUsers();
+
+        return res.status(200).send(allUsers)
+    } catch(err){
+    this.logger.error(err.message);
+    throw new HttpException(err.message,err.status);
+    };
+  };
+
+  @Post("/v1/allUsers")
+  private async createUser(@Body()data:CreateUserDTO,@Res()res:Response):Promise<Response>{
+    try{
+
+        // const realPassword
+
+        const createUser = await this.userService.insertUser(data);
+
+        return res.status(201).send(createUser);
+    }catch(err){
+    this.logger.error(err.message);
+    throw new HttpException(err.message,err.status);
+    };
+  };
+};
