0a1a3de743ebc92d2e6e608849e787a030029895
Gabriel Castro <estudosbabiel@gmail.com>
2025-03-01 10:50:50 -0300

    config: Configurating the Redis into the user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class UserService {
     private prisma;
     private logger = new Logger(UserService.name)
-    constructor(private pr:PrismaService){
+    constructor(private pr:PrismaService,private redisService:RedisService){
         this.prisma = pr.user;
     };
+
 };
