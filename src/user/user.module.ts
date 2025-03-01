1b3fc970be2bac1f1ed1226acd22146f99463734
Gabriel Castro <estudosbabiel@gmail.com>
2025-03-01 10:47:00 -0300

    config: Importing the correct dependencies

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { RedisModule } from 'src/redis/redis.module';
+import { BullModule } from '@nestjs/bull';
+import { USER_QUEUE } from 'src/constants/constants';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
+  imports:[
+    RedisModule,
+    PrismaModule,
+    BullModule.registerQueue({
+      name:USER_QUEUE,
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
