6606f217a1d186b56f1316df7a1eb9be09ce08df
Gabriel Castro <estudosbabiel@gmail.com>
2025-03-01 10:28:35 -0300

    feat: Made some logs if the redis is on or off

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService extends Redis{
     constructor(){
+        const logger = new Logger(RedisService.name);
         super();
-    }
-}
+        if(super.on){
+            logger.log("Redis dependencies on!");
+        };
+        
+        if(super.off){
+            logger.error("Redis dependencies off, please restart your program!");
+        };
+        
+    };
+};
