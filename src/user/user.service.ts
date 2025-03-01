e161a0dbd05f7b8da08ee0a477cf04ead44cad72
Gabriel Castro <estudosbabiel@gmail.com>
2025-03-01 10:50:00 -0300

    config: Configurating the prisma into the user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private prisma;
+    private logger = new Logger(UserService.name)
+    constructor(private pr:PrismaService){
+        this.prisma = pr.user;
+    };
+};
