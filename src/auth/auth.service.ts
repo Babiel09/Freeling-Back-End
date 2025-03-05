1d33585a502c9f4aff110aa0e452e4d3738610ee
Gabriel Castro <estudosbabiel@gmail.com>
2025-03-05 16:05:34 -0300

    feat: Created new method for the login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,39 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
     private logger = new Logger(AuthService.name);
-    constructor(private jwtService:JwtService, userService:UserService){};
+    constructor(private jwtService:JwtService, private userService:UserService){};
 
-    public async login(email:string,password:string){};
+    public async login(email:string,password:string){
+        try{
+            const userFindedByEmail = await this.userService.findUserByEmail(email);
+
+            const verifyPassword = await bcrypt.verifyPassword(password,userFindedByEmail.password);
+
+            if(verifyPassword){
+                this.logger.error("Invalid creentials!");
+                throw new HttpException("Invalid creentials!",401);
+            };
+
+            const payload:object = {
+                id:userFindedByEmail.id,
+                name:userFindedByEmail.name,
+                email:userFindedByEmail.email,
+                password:userFindedByEmail.password,
+                photo:userFindedByEmail.photo,
+                workName:userFindedByEmail.workName
+            };
+
+            const createToken = await this.jwtService.signAsync(payload);
+
+            return JSON.parse(createToken);
+
+        }catch(err){
+            this.logger.error(err.message);
+            throw new HttpException(err.message,err.status);
+        };
+    };
 };
